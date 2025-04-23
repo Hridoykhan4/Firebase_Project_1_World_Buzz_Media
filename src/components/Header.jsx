f7f0d686be653e95fee409b32c1e39445e0de105
Hridoykhan4 <hridoykhan148385@gmail.com>
2025-04-23 06:24:14 +0600

    UI modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className="flex justify-center items-center flex-col gap-2 py-2">
       <div className="logo">
-        <img className="w-[7rem] rounded-lg dark:bg-white/40" src={logo3} alt="" />
+        <img className="w-[7rem] rounded-full object-cover  dark:bg-white/40" src={logo3} alt="" />
       </div>
       <p className="">One World. One Feed.</p>
       <p className="font-medium">{moment().format(`dddd, MMMM Do, YYYY`)}</p>
